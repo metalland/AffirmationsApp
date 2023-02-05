bbfe0058adb159c04c217940c8743e7bf9db2ffd
metal_land <kirill.bronovicki@gmail.com>
2023-02-05 15:41:17 +0100

    add simple tests

--- app/src/test/java/io/github/affirmationsapp/ExampleUnitTest.kt
+++ app/src/test/java/io/github/affirmationsapp/ExampleUnitTest.kt
@@ -1,8 +1,12 @@
 package io.github.affirmationsapp
 
+import android.content.Context
+import io.github.affirmationsapp.adapter.ItemAdapter
+import io.github.affirmationsapp.model.Affirmation
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.mockito.Mockito.mock
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +14,18 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+
+    private val context = mock(Context::class.java)
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun adapter_size() {
+        val data = listOf(
+            Affirmation(R.string.affirmation1, R.drawable.image1),
+            Affirmation(R.string.affirmation2, R.drawable.image2)
+        )
+
+        assertEquals(2, ItemAdapter(context, data).itemCount)
     }
+
+
 }
